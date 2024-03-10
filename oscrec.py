720d0c983b750e780204ce2cbca20fddd19c4dd7
Shane Hutter <shane@hal9001.intentropycs.com>
2024-03-10 12:21:20 +1100

    Still working on PoC

--- oscrec.py
+++ oscrec.py
@@ -4,10 +4,38 @@
 from jack   import Client
 from yaml   import safe_dump
 from time   import sleep
+from scipy.io.wavfile   import write
+from numpy              import (
+    append, ndarray,
+    int16,  int32,
+    )
 
 def test_hold():
     while True:
         sleep( 1 )
+
+def print_inport_stream( inports , samplerate ):
+    """This is a test function to print out the array per frame from inports"""
+    while True:
+        for name , inport in inports.items():
+            if "1" in name:
+                print( type( inport.get_array() ) )
+        exit()
+        sleep( 100000/ samplerate )
+              
+def test_rec( inports , samplerate ):
+    """Test record and save foo.wav"""
+    _array_data = ndarray( [] )
+    for x in range( int( samplerate /10 ) ):
+        for name , inport in inports.items():
+            if "1" in name:
+                #print( x / samplerate )
+                _array_data = append( _array_data , inport.get_array() )
+    write( "foobar.wav" , samplerate , _array_data )
+    
+
+
+
 
 if __name__ == '__main__':
     # Create client
@@ -17,7 +45,6 @@ if __name__ == '__main__':
         client_data = {
             "blocksize": client.blocksize,
             "sample_rate": client.samplerate,
-            "status": client.status,
             "uuid":  client.uuid,
             }
 
@@ -46,4 +73,6 @@ if __name__ == '__main__':
         print( safe_dump( client_data ) )
         print( outports )
         print( inports )
-        test_hold()
+        #test_hold()
+        #print_inport_stream( inports=inports , samplerate = client.samplerate )
+        test_rec( inports=inports , samplerate = client.samplerate )
